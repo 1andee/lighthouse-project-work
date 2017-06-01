39c4474f7a8f343500231a9a0015ab18adaa3512
Ian Duke <ian.c.duke@gmail.com>
2017-06-01 20:14:41 +0000

    Successfully sorting by name, but also need to duplicate ages by name. Almost there.

---
+++ week1/w1d4/custom_sort.js
@@ -0,0 +1,32 @@
+// TASK ONE
+var array = [10, 2, 5, 1, 9];
+
+var sortaroo = array.sort(function (a,b) {
+    return a - b;
+});
+
+console.log(sortaroo);
+
+
+
+//  TASK TWO
+var students = [
+    { id: 1, name: "bruce",    age: 40 },
+    { id: 2, name: "zoidberg", age: 22 },
+    { id: 3, name: "alex",     age: 22 },
+    { id: 4, name: "alex",     age: 30 }
+  ];
+
+var sortedStudentList = students.sort(function(a, b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+
+  // names must be equal
+  return 0;
+});
+
+console.log(sortedStudentList);
