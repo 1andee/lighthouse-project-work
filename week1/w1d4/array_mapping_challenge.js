8e3c3d010956e25a7e5fbc987600201b840d6287
Ian Duke <ian.c.duke@gmail.com>
2017-06-01 19:00:33 +0000

    Completed and tested this script successfully

---
+++ week1/w1d4/array_mapping_challenge.js
@@ -0,0 +1,19 @@
+// Pair programmed with Katia on 01 June 2017
+//
+// If you recall Pythagorean Theorem (x^2 + y^2 = z^2), then this should be a breeze.
+//
+// Complete the following code for the assertion to pass. The result should be an array of numbers corresponding to the x-y pairs provided in the input array (ie: calculate z given x and y).
+
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+var result = input.map(function(num) {
+  return Math.sqrt((num.x * num.x) + (num.y * num.y));
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
