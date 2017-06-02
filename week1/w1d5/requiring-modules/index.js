92fce9237b7d1aaa14f704c11376f7bcb8d99181
Ian Duke <ian.c.duke@gmail.com>
2017-06-02 15:51:01 +0000

    Invokes two exported functions, with private functions invoked in module.

--- week1/w1d5/requiring-modules/index.js
+++ week1/w1d5/requiring-modules/index.js
@@ -2,8 +2,7 @@
 var multiplication = require("./multiplication");
 
 // Call the timesTwo function in multiplication.js, passing in 500 as argument
-
 // console.log(multiplication.timesTwo(500));
 // Practice using template literal:
-console.log(`500 times two is ${multiplication.timesTwo(500)}`);
-console.log(`The square root of 3 is ${multiplication.squareRoot(3)}`);
+console.log(multiplication.logTimesTwo(2));
+console.log(multiplication.logSquareRoot(3));
--- week1/w1d5/requiring-modules/multiplication.js
+++ week1/w1d5/requiring-modules/multiplication.js
@@ -1,12 +1,20 @@
 function timesTwo(num) {
- return (num * 2);
+  return num * 2;
+}
+
+function logTimesTwo(userNum) {
+  console.log(`${userNum} times two is ${timesTwo(userNum)}`);
 }
 
 function squareRoot(num) {
   return (num * num);
 }
 
+function logSquareRoot(userNum) {
+  console.log(`The square root of ${userNum} is ${squareRoot(userNum)}`)
+}
+
 module.exports = {
-  timesTwo: timesTwo,
-  squareRoot: squareRoot
+  logTimesTwo: logTimesTwo,
+  logSquareRoot: logSquareRoot
 }
