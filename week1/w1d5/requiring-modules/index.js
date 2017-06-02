35165252aa573dbdb7577a7aabfe573d9cb938d3
Ian Duke <ian.c.duke@gmail.com>
2017-06-02 15:29:44 +0000

    Updated the code after practicing a bit more

--- week1/w1d5/requiring-modules/index.js
+++ week1/w1d5/requiring-modules/index.js
@@ -2,7 +2,8 @@
 var multiplication = require("./multiplication");
 
 // Call the timesTwo function in multiplication.js, passing in 500 as argument
-console.log(multiplication.timesTwo(500));
 
-// OR IF USING TEMPLATE LITERAL:
-// console.log(`500 times two is ${multiplication.timesTwo(500)}`);
+// console.log(multiplication.timesTwo(500));
+// Practice using template literal:
+console.log(`500 times two is ${multiplication.timesTwo(500)}`);
+console.log(`The square root of 3 is ${multiplication.squareRoot(3)}`);
--- week1/w1d5/requiring-modules/multiplication.js
+++ week1/w1d5/requiring-modules/multiplication.js
@@ -2,6 +2,11 @@ function timesTwo(num) {
  return (num * 2);
 }
 
+function squareRoot(num) {
+  return (num * num);
+}
+
 module.exports = {
   timesTwo: timesTwo,
+  squareRoot: squareRoot
 }
