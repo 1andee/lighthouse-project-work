499ff2725c02ae2346876b83442b025eada0366a
Ian Duke <ian.c.duke@gmail.com>
2017-06-04 12:53:00 -0400

    Got it working in Node

---
+++ week1/w1d1/reverse.js
@@ -0,0 +1,21 @@
+/* Write a program that takes any number of command line arguments,
+all strings, and reverses them before outputting them one at a time
+to the console.
+
+Do NOT use JavaScript's Array.prototype.reverse or Array.prototype.join
+functions to solve this problem. */
+
+var myArgs = process.argv.slice(2);
+
+function reverse(myArgs) {
+
+  myArgs.forEach(function (element) {
+    var reversedWord = "";
+    for (i = element.length - 1; i >= 0; i--) {
+      reversedWord += element[i];
+    }
+    console.log(reversedWord);
+  });
+}
+
+reverse(myArgs);
