a582ed29d3ef1d171cfb3a9eb7833586c7956ef8
Ian Duke <ian.c.duke@gmail.com>
2017-06-05 11:40:59 -0400

    Completed Step 4

---
+++ week2/w2d1/scripting-http-requests/4_getHTML.js
@@ -0,0 +1,43 @@
+/* PRINTING WITH CALLBACKS
+
+Create a fourth function, getHTML. This function will look and behave almost the
+same as your third function, except:
+
+- The function definition will accept a callback as a second parameter.
+- The function body will invoke (call) the callback when the data is fully
+received, in place of the original console.log.
+- Ensure the callback function printHTML gets passed as an argument when you call getHTML */
+
+var https = require('https');
+
+function getHTML (options, callback) {
+
+  https.get(options, function (response) {
+
+    var html = [];
+    response.setEncoding('utf8');
+
+    console.log('[--START OF REQUESTED HTML--]');
+
+    response.on('data', function (data) {
+      html.push(data);
+    });
+
+    response.on('end', function() {
+      console.log(html.join(""));
+      console.log('[--END OF HTML--].');
+    });
+
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
