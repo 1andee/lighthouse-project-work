37b1e1782f82e90e5c3b9f2d4228e4fbcd7f8a76
Ian Duke <ian.c.duke@gmail.com>
2017-07-05 14:15:17 +0000

    Finishing up...

--- week6/w6d1/candidates/filters.rb
+++ week6/w6d1/candidates/filters.rb
@@ -1,16 +1,49 @@
+# Enumeration with Candidates
+# Lighthouse Labs web-ft-tor-29may2017
+#
 # In this file we define the methods to help filter out candidates
-# This way, we keep these methods separated from other potential parts of the program
-
-def find(id)
-  # Your code Here
-end
+# This way, we keep these methods separated from other potential parts
+# of the program
 
 def experienced?(candidate)
-  # Your code Here
+  candidate[:years_of_experience] >= 2
 end
 
-def qualified_candidates(candidates)
-  # Your code Here
+def find(id)
+  @candidates.find {|candidate| candidate[:id] == id}
 end
 
-# More methods will go below
+def enough_pts?(candidate)
+  candidate[:github_points] >= 100
+end
+
+def language?(candidate)
+  candidate[:languages].include?('Ruby' || 'Python')
+end
+
+def applied_recently?(candidate)
+  candidate[:date_applied] >= 15.days.ago.to_date
+end
+
+def old_enough?(candidate)
+  candidate[:age] >= 18
+end
+
+def qualified_candidates
+  @candidates.select do |candidate|
+    if experienced?(candidate) == true &&
+      enough_pts?(candidate) == true &&
+      language?(candidate) == true &&
+      applied_recently?(candidate) == true &&
+      old_enough?(candidate) == true
+      pp candidate
+    end
+  end
+end
+
+# more methods will go here:
+
+def sort_by_qualifications(candidates)
+  # sort by years_of_experience
+  # then sort by github_points
+end
--- week6/w6d1/candidates/main.rb
+++ week6/w6d1/candidates/main.rb
@@ -5,8 +5,44 @@ require 'pry'
 require './candidates'
 require './filters'
 
-## Your test code can go here
+binding.pry
 
-# binding.pry
-
+# List all candidates ('pretty print')
+puts "-----------------------------"
+puts "Show all candidates:"
 pp @candidates
+
+# Return TRUE if candidate has > 2 yrs experience
+puts "-----------------------------"
+puts "Return TRUE if candidate has > 2 yrs experience"
+@candidates.each {|candidate| puts experienced?(candidate)}
+
+# Lookup candidate by id
+puts "-----------------------------"
+puts "Looking up candidate by ID:"
+puts find(9)
+
+# Return TRUE if candidate has > 100 github points
+puts "-----------------------------"
+puts "Return TRUE if candidate has > 100 github points"
+@candidates.each {|candidate| puts enough_pts?(candidate)}
+
+# Return TRUE if candidate knows Ruby or Python
+puts "-----------------------------"
+puts "Return TRUE if candidate knows Ruby or Python"
+@candidates.each {|candidate| puts language?(candidate)}
+
+# Return TRUE if candidate applied within past 15 days
+puts "-----------------------------"
+puts "Return TRUE if candidate applied within past 15 days"
+@candidates.each {|candidate| puts applied_recently?(candidate)}
+
+# Return TRUE if candidate is over 18 years old
+puts "-----------------------------"
+puts "Return TRUE if candidate is over 18 years old"
+@candidates.each {|candidate| puts old_enough?(candidate)}
+
+# List only the qualified candidiates
+puts "-----------------------------"
+puts "List of only the qualifed candidates:"
+pp qualified_candidates
