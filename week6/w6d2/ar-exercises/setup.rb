fe6b2a3946bf1319373719d8eb4ba38e5c40b13e
Ian Duke <ian.c.duke@gmail.com>
2017-07-06 11:34:39 -0400

    Created ENV file and gitignore

--- week6/w6d2/ar-exercises/setup.rb
+++ week6/w6d2/ar-exercises/setup.rb
@@ -1,4 +1,4 @@
-require 'pry' # in case you want to use binding.pry
+# require 'pry' # in case you want to use binding.pry
 require 'active_record'
 require_relative 'lib/store'
 require_relative 'lib/employee'
@@ -9,11 +9,11 @@ ActiveRecord::Base.logger = Logger.new(STDOUT)
 puts 'Establishing connection to database ...'
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
-  database: 'ar_exercises',
-  username: 'development',
-  password: 'development',
-  host: 'localhost',
-  port: 5432,
+  database: ENV['DB_NAME'],
+  username: ENV['DB_USER'],
+  password: ENV['DB_PASS'],
+  host: ENV['DB_HOST'],
+  port: ENV['DB_PORT'],
   pool: 5,
   encoding: 'unicode',
   min_messages: 'error'
