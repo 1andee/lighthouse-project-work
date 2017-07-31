7000905eee77e4fd62c0a230eab6d3c962cbe931
Ian Duke <ian.c.duke@gmail.com>
2017-07-31 17:26:17 -0400

    Remove in-line comments (what was I thinking?)

--- week1/w1d3/countLetters.js
+++ week1/w1d3/countLetters.js
@@ -1,17 +1,21 @@
-
 function countLetters(string) {
-  objectOfChars = {};                                                 // Creates an empty object
-  stringArray = string.split(" ").join("").toLowerCase().split("");   // Converts string into array of characters
+  objectOfChars = {};
 
-  for (var i = 0; i < stringArray.length; i++) {                      // Create for loop to iterate through each array item
-    var currentChar = stringArray[i];                                 // Sets current array item to variable currentChar
-    if (objectOfChars[currentChar] == undefined) {                    // If character doesn't exist in object, set count to one
+  // Converts string into array of characters
+  stringArray = string.split(" ").join("").toLowerCase().split("");
+
+  // Iterate through array
+  for (var i = 0; i < stringArray.length; i++) {
+    var currentChar = stringArray[i];
+    // If !character in object, set value to one
+    if (objectOfChars[currentChar] == undefined) {
       objectOfChars[currentChar] = 1;
-      } else {
-      objectOfChars[currentChar]++;                               // If character already exists in object, increment by one
-      }
-  }
-  return objectOfChars;                                             // Return all of the characters
-}
+    } else {
+      // Increment characters that already exist in object
+      objectOfChars[currentChar]++;
+    };
+  };
+  return objectOfChars;
+};
 
 console.log(countLetters("Lighthouse in the house"));
